126209fd25cc46813ca7a744142bd8e24e330aa8
Amber Kapoor <32417551+Amberkapoor@users.noreply.github.com>
2022-12-14 15:04:43 -0500

    Add files via upload

---
+++ ChatMessage.java
@@ -0,0 +1,50 @@
+package com.example.macstudent.chatapp;
+
+/**
+ * Created by macstudent on 2017-12-05.
+ */
+import java.util.Date;
+
+
+public class ChatMessage {
+
+    private String messageText;
+    private String messageUser;
+    private long messageTime;
+
+    public ChatMessage(String messageText, String messageUser) {
+        this.messageText = messageText;
+        this.messageUser = messageUser;
+
+
+        messageTime = new Date().getTime();
+    }
+
+    public ChatMessage(){
+
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getMessageUser() {
+        return messageUser;
+    }
+
+    public void setMessageUser(String messageUser) {
+        this.messageUser = messageUser;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+
+    public void setMessageTime(long messageTime) {
+        this.messageTime = messageTime;
+    }
+}
